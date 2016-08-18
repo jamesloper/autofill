ef64de8ff2045db78283f2c79e7032fcb591ca65
James <james@hux.com>
2016-08-18 09:19:23 -0400

    initial commit

---
+++ client/main.js
@@ -0,0 +1,35 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import './main.html';
+
+Template.AutofillDemo.onCreated(function() {	
+	this.search = new ReactiveVar();
+	this.results = new ReactiveVar();
+	
+	this.autorun(() => {
+		var search = this.search.get();
+		Meteor.call('Autofill', search, function(err, res) {
+			if (err) {
+				console.log('ERROR:', err);
+			} else {
+				console.log('RESULT:', res);
+			}
+		});
+	});
+});
+
+Template.AutofillDemo.helpers({
+	search() {
+		return Template.instance().search.get();
+	},
+	results() {
+		return Template.instance().results.get();
+	},
+});
+
+Template.AutofillDemo.events({
+	'change input, keyup input': _.throttle(function(e, tpl) {
+		tpl.search.set(e.target.value);
+	}, 250),
+});
---
+++ server/main.js
@@ -0,0 +1,18 @@
+import { Meteor } from 'meteor/meteor';
+
+Meteor.methods({
+	'Autofill': function(search) {
+		var BEGIN = Date.now();
+		
+		// api calls and shit
+		// api calls and shit
+		// api calls and shit
+		// api calls and shit
+		// api calls and shit
+		// api calls and shit
+		// api calls and shit
+		
+		var EXEC = Date.now() - BEGIN;
+		console.log(`Search "${search}" completed in ${exec} ms.`);
+	}
+});
