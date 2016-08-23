62576279289ac00f4954bdc6c641664bcf872eeb
Spencer Applegate <spencer@spencerapplegate.com>
2016-08-23 19:19:44 -0400

    Added a bit of styling

--- server/main.js
+++ server/main.js
@@ -12,18 +12,28 @@ getDetail = function(prediction, callback) {
 	};
 
 	HTTP.call('GET', url, params, function(err, res) {
-		if (err) {
-			return callback(err);
-		} else {
-			return callback(null, res.data.result.formatted_address);
-		}
+		if (err) return callback(err);
+
+		var data = { };
+		if (res.data.result) {
+			data.address = res.data.result.formatted_address;
+			if (res.data.result.geometry) {
+				data.coords = [res.data.result.geometry.location.lat, res.data.result.geometry.location.lng];
+			};
+		};
+		return callback(null, data);
 	});
 }
 
 getDetails = function(predictions, callback) {
 	syncGetDetail = Meteor.wrapAsync(getDetail);
 	var details = _.map(predictions, function(prediction) {
-		return { text: formatAddress(syncGetDetail(prediction)) };
+		detail = syncGetDetail(prediction);
+		if (!detail.coords || !detail.address) return;
+		return {
+			coords: detail.coords,
+			text: formatAddress(detail.address)
+		};
 	});
 	return callback(null, details);
 }
@@ -44,17 +54,40 @@ asyncFunc = function(search, callback) {
 	syncGetDetails = Meteor.wrapAsync(getDetails);
 
 	HTTP.call('GET', url, params, function(err, res) {
-		if (err) {
-			return callback(err);
-		} else {
-			var predictions = res.data.predictions;
-			return callback(null, syncGetDetails(predictions));
-		}
+		if (err) return callback(err);
+
+		var data = _.compact(_.map(syncGetDetails(res.data.predictions), function(datum) {
+			if (!datum) return;
+			return {
+				text: datum.text,
+				distance: distanceBetween(datum.coords, _.map(params.params.location.split(','), function(cord) {
+					return parseFloat(cord);
+				}))
+			};
+		}));
+
+		return callback(null, _.sortBy(data, 'distance'));
 	});
 };
 
 formatAddress = function(address) {
+	if (!address) return null;
 	return address.split(',').slice(0, -1).join(', ');
+}
+
+// Utilizing Haversine formula: https://en.wikipedia.org/wiki/Haversine_formula
+distanceBetween = function(x, y) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = (y[0] - x[0]) * (Math.PI / 180);
+  var dLon = (y[1] - x[1]) * (Math.PI / 180);
+  var a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(x[0] * (Math.PI / 180)) * Math.cos(y[0] * (Math.PI / 180)) *
+    Math.sin(dLon / 2) * Math.sin(dLon / 2)
+    ;
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  var d = R * c; // Distance in km
+  return d;
 }
 
 Meteor.methods({
